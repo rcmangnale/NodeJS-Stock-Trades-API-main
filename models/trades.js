d36dbabe804c5d7a609a4dff0b31626face453f3
Rameshwar Mangnale <mangnalercm97@gmail.com>
2022-03-30 09:38:13 +0530

    changes update

--- models/trades.js
+++ models/trades.js
@@ -1,11 +1,5 @@
-// Uncomment the code below to use Sequelize ORM
-// const {Sequelize} = require("sequelize");
-// const sequelize = new Sequelize("sqlite::memory:");
 
-// Uncomment the code below to use Mongoose ORM
 const mongoose = require("mongoose");
-
-// Insert your model definition below
 
 const tradeSchema = new mongoose.Schema({
   id: { type: Number },
